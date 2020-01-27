dc9f46773415a3a8f1dce6a2ad3e1cf6a614c308
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-01-27 16:43:20 -0300

    Formatando calendario de eventos

--- my-todo-list/src/Todo-List/Agenda.js
+++ my-todo-list/src/Todo-List/Agenda.js
@@ -1,85 +1,150 @@
-import React from 'react';
-import { ReactAgenda , ReactAgendaCtrl , guid ,  Modal } from 'react-agenda';
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-//https://github.com/Revln9/react-agenda
+const localizer = momentLocalizer(moment);
 
+class Agenda extends Component {
+  constructor() {
+    super();
+    const now = new Date();
+    const events = [
+      {
+          id: 0,
+          title: 'All Day Event very long title',
+          allDay: true,
+          start: new Date(2015, 3, 0),
+          end: new Date(2015, 3, 1),
+      },
+      {
+          id: 1,
+          title: 'Long Event',
+          start: new Date(2015, 3, 7),
+          end: new Date(2015, 3, 10),
+      },
 
-require('moment/locale/pt.js'); // this is important for traduction purpose
+      {
+          id: 2,
+          title: 'DTS STARTS',
+          start: new Date(2016, 2, 13, 0, 0, 0),
+          end: new Date(2016, 2, 20, 0, 0, 0),
+      },
 
- 
-var colors= {
-  'color-1':"rgba(102, 195, 131 , 1)" ,
-  "color-2":"rgba(242, 177, 52, 1)" ,
-  "color-3":"rgba(235, 85, 59, 1)"
-}
- 
-var now = new Date();
- 
-var items = [
-  {
-   _id            :guid(),
-    name          : 'Meeting , dev staff!',
-    startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0),
-    endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 0),
-    classes       : 'color-1'
-  },
-  {
-   _id            :guid(),
-    name          : 'Working lunch , Holly',
-    startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 11, 0),
-    endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 13, 0),
-    classes       : 'color-2 color-3'
-  },
- 
-];
- 
-export default class Agenda extends React.Component {
-  constructor(props){
-  super(props);
+      {
+          id: 3,
+          title: 'DTS ENDS',
+          start: new Date(2016, 10, 6, 0, 0, 0),
+          end: new Date(2016, 10, 13, 0, 0, 0),
+      },
+
+      {
+          id: 4,
+          title: 'Some Event',
+          start: new Date(2015, 3, 9, 0, 0, 0),
+          end: new Date(2015, 3, 10, 0, 0, 0),
+      },
+      {
+          id: 5,
+          title: 'Conference',
+          start: new Date(2015, 3, 11),
+          end: new Date(2015, 3, 13),
+          desc: 'Big conference for important people',
+      },
+      {
+          id: 6,
+          title: 'Meeting',
+          start: new Date(2015, 3, 12, 10, 30, 0, 0),
+          end: new Date(2015, 3, 12, 12, 30, 0, 0),
+          desc: 'Pre-meeting meeting, to prepare for the meeting',
+      },
+      {
+          id: 7,
+          title: 'Lunch',
+          start: new Date(2015, 3, 12, 12, 0, 0, 0),
+          end: new Date(2015, 3, 12, 13, 0, 0, 0),
+          desc: 'Power lunch',
+      },
+      {
+          id: 8,
+          title: 'Meeting',
+          start: new Date(2015, 3, 12, 14, 0, 0, 0),
+          end: new Date(2015, 3, 12, 15, 0, 0, 0),
+      },
+      {
+          id: 9,
+          title: 'Happy Hour',
+          start: new Date(2015, 3, 12, 17, 0, 0, 0),
+          end: new Date(2015, 3, 12, 17, 30, 0, 0),
+          desc: 'Most important meal of the day',
+      },
+      {
+          id: 10,
+          title: 'Dinner',
+          start: new Date(2015, 3, 12, 20, 0, 0, 0),
+          end: new Date(2015, 3, 12, 21, 0, 0, 0),
+      },
+      {
+          id: 11,
+          title: 'Birthday Party',
+          start: new Date(2015, 3, 13, 7, 0, 0),
+          end: new Date(2015, 3, 13, 10, 30, 0),
+      },
+      {
+          id: 12,
+          title: 'Late Night Event',
+          start: new Date(2015, 3, 17, 19, 30, 0),
+          end: new Date(2015, 3, 18, 2, 0, 0),
+      },
+      {
+          id: 12.5,
+          title: 'Late Same Night Event',
+          start: new Date(2015, 3, 17, 19, 30, 0),
+          end: new Date(2015, 3, 17, 23, 30, 0),
+      },
+      {
+          id: 13,
+          title: 'Multi-day Event',
+          start: new Date(2015, 3, 20, 19, 30, 0),
+          end: new Date(2015, 3, 22, 2, 0, 0),
+      },
+      {
+          id: 14,
+          title: 'Today',
+          start: new Date(new Date().setHours(new Date().getHours() - 3)),
+          end: new Date(new Date().setHours(new Date().getHours() + 3)),
+      },
+      {
+          id: 15,
+          title: 'Point in Time Event',
+          start: now,
+          end: now,
+      },
+    ]
     this.state = {
-      items:items,
-      selected:[],
-      cellHeight:30,
-      showModal:false,
-      locale:"pt",
-      rowsPerHour:2,
-      numberOfDays:4,
-      startDate: new Date()
-    }
-    this.handleCellSelection = this.handleCellSelection.bind(this)
-    this.handleItemEdit = this.handleItemEdit.bind(this)
-    this.handleRangeSelection = this.handleRangeSelection.bind(this)
+      name: 'React',
+      events
+    };
   }
- 
-handleCellSelection(item){
-  console.log('handleCellSelection',item)
-}
-handleItemEdit(item){
-  console.log('handleItemEdit', item)
-}
-handleRangeSelection(item){
-  console.log('handleRangeSelection', item)
-}
+
   render() {
     return (
       <div>
-        <ReactAgenda
-          minDate={now}
-          maxDate={new Date(now.getFullYear(), now.getMonth()+3)}
-          disablePrevButton={false}
-          startDate={this.state.startDate}
-          cellHeight={this.state.cellHeight}
-          locale={this.state.locale}
-          items={this.state.items}
-          numberOfDays={this.state.numberOfDays}
-          rowsPerHour={this.state.rowsPerHour}
-          itemColors={colors}
-          autoScale={false}
-          fixedHeader={true}
-          onItemEdit={this.handleItemEdit.bind(this)}
-          onCellSelect={this.handleCellSelection.bind(this)}
-          onRangeSelection={this.handleRangeSelection.bind(this)}/>
+        <p>
+          A test for the React Big Calendar.
+        </p>
+        <div style={{ height: '240pt'}}>
+          <Calendar
+            events={this.state.events}
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={moment().toDate()}
+            localizer={localizer}
+          />
+        </div>
       </div>
     );
   }
 }
+
+export default Agenda;
--- my-todo-list/src/Todo-List/Calendario.js
+++ my-todo-list/src/Todo-List/Calendario.js
@@ -1,25 +1,70 @@
 import React, {Component} from 'react';
-import Calendar from 'react-calendar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
 class Calendario extends Component {
 
     constructor(props) {
         super(props);
+        this.onSubmit = this.onSubmit.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
+
+        this.state = {tarefas: []}
     }
 
-    state = {
-        data: new Date(),
+    onSubmit(event){
+        event.preventDefault();
+        var novoItem = this.refs.tarefa.value;
+        
+        if(novoItem) {
+            var vetItens = this.state.tarefas.concat(novoItem)
+            this.setState({tarefas: vetItens})           
+            this.refs.form.reset();
+        }
     }
+
+    deleteItem(index) {
+        var clone = Array.from(this.state.tarefas)
+        clone.splice(index, 1)
+        this.setState({ tarefas: clone })
+    }
+    
 
     render() {
         return (
             <div className="corpo">
                 <center>
-                    <h1>Calendario</h1>
-                    
-                    <Calendar 
-                        onChange={this.onChange}
-                        value={this.state.date}/>
+                    <h1>Adicionar eventos ao calendário</h1>
+
+                    <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+                        
+                        <label for="nova_tarefa">Nome da tarefa: </label>
+                            <input type="text" ref="tarefa" placeholder="Digite a tarefa aqui"></input>
+                        <br />
+                        <label for="data_inicio">Data Início: </label>
+                            <input type="date" ref="data-inicio" min="2020-01-01" max="2099-12-31"></input>
+                        
+                        <label for="hora_inicio">Hora Início: </label>
+                            <input type="number" ref="hora_inicio" min="1" max="12"></input>:<input type="number" ref="minuto_inicio" min="00" max="59"></input>
+                        <br />
+                        <label for="data_inicio">Data Fim: </label>
+                            <input type="date" ref="data-inicio" min="2020-01-01" max="2099-12-31"></input>
+                        
+                        <label for="hora_fim">Hora Fim: </label>
+                            <input type="number" ref="hora_fim" min="1" max="12"></input>:<input type="number" ref="minuto_fim" min="00" max="59"></input>
+                        <br />
+
+                        <input type="submit" name="btnAdd" value="Continuar"/>
+                        <br /><br />
+
+                        {this.state.tarefas.map((item, index) => {
+                            return <li key={index} onClick={() => this.deleteItem(index)}>{item}</li>
+                        })}
+
+                        <br />
+                        <input type="submit" name="btnSend" value="Adicionar Evento"/>
+
+                    </form>
                 </center>
             </div>
         );
@@ -27,3 +72,6 @@ class Calendario extends Component {
 }
 
 export default Calendario;
+
+//listar o novo evento antes de incluir no calendário, se user confirmar, envia para o calendario
+//criar um component para passar as informações do this pro calendario
